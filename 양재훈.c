228c79fa56bbf8adb895dcbd9af793f53b9cab96
jea_hu_ni <98939922+jeaH0N@users.noreply.github.com>
2022-03-13 22:57:36 +0900

    Add files via upload

---
+++ 양재훈.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+//자기이름 10번 출력하기(for구문) 
+//int main(void)
+//{
+//	int i = 0;
+	// for (초기식; 조건식; 증감식){} 
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("양재훈\n");
+//	}
+//}
+
+//두수 의 합
+//double GetSumTwoNumber(double x, double y)
+//{
+//	double r = x + y;
+//	return r;
+//}
+//int main(void)
+//{
+//	printf("%.2f\n", GetSumTwoNumber(3,5));
+//	printf("%.2f\n", GetSumTwoNumber(3,0.14));	
+//	return 0;
+// } 
+
+
+//1부터 100까지 합하기 (while) 구문 사용하기 
+//int main(void)
+//{
+//	int i = 1;
+//	int sum = 0;
+	
+	
+//	while( i <= 100)
+//	 {
+//	 	sum += i;
+//		i++;
+//	}
+//	printf("최종값은 : %d",sum);
+//	return 0;
+// }
+//구구단 (for) 구문 사용하기 
+//int main(void)
+//{
+//	int i, j;
+	
+//	for(j = 1; j <= 9; j++)
+//	{
+//		for(i = 2;i <= 9;i++)
+//		{
+//			printf("%d x %d = %2d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//원의 지름과 면적구하기 
+//int main(void)
+
+//{
+//	float r;//원의 반지름
+//	float a;//원의 면적
+//	int d;//원의 지름
+	 
+//	printf("반지름의 값을 입력하시오 >> ");
+//	scanf("%f", &r);
+//	d = r * 2 ;
+//	a =  3.14 * r * r ;
+//	printf("원의 면적 = %f, 원의 지름은 = %d", a , d);
+	
+//	return 0;
+//}
